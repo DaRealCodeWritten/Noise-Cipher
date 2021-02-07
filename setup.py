435deb7c17b584dfe1da33221fa1b3e6d2ccd0d3
DaRealCodeWritten <SSwiftGamer440@gmail.com>
2021-02-07 01:13:06 -0800

    Update and rename setup.cfg to setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Noise-Cipher-CodeWritten",
+    version="0.0.2",
+    author="CodeWritten",
+    author_email="null",
+    description="A new cipher module",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DaRealCodeWritten/Noise-Cipher",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
