1847c33a0f473ec148964690217063239623cba0
DaRealCodeWritten <SSwiftGamer440@gmail.com>
2021-02-07 01:44:28 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="Noise-Cipher-CodeWritten",
-    version="0.0.2",
+    version="0.0.3",
     author="CodeWritten",
     author_email="ytcodew@gmail.com",
     description="A new cipher module",
