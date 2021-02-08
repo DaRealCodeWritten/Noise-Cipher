5d56e61bb1b808abbc9492f6253877e94bd5b804
DaRealCodeWritten <SSwiftGamer440@gmail.com>
2021-02-08 02:39:25 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="noise-cipher",
-    version="0.1.0",
+    version="0.1.1",
     author="CodeWritten",
     author_email="ytcodew@gmail.com",
     description="A new cipher module",
